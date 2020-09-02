1e042be40a81ae270cf36e4c0da233c1b7e04371
David Seo <manakete659@gmail.com>
2020-09-02 19:39:44 -0400

    Added feedback on successful addition and error on deletion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import PhoneService from './PhoneService';
 
+const Notification = ({ style, message }) => {
+
+  if (message === null) {
+    return null
+  }
+  return (
+    <div style={style}>{message}</div>
+  )
+}
+
 const Filter = ({ string, method }) => {
   return (
     <>
@@ -32,9 +42,12 @@ const Persons = ({ filterString, arrayList, deleteMethod }) => {
     <>
       {
         filterString.length === 0 ?
-          arrayList.map(person => <p key={person.id}>{person.name} {person.number} <button value={person.id} type="button" onClick={deleteMethod}>delete</button></p>) :
-          arrayList.filter(person => person.name.toLowerCase().includes(filterString.toLowerCase()))
-            .map(person => <p key={person.id}>{person.name} {person.number} <button value={person.id} type="button" onClick={deleteMethod}>delete</button></p>)
+          arrayList
+            .map(person =>
+              <p key={person.id}>{person.name} {person.number} <button value={JSON.stringify(person)} type="button" onClick={deleteMethod}>delete</button></p>) :
+          arrayList
+            .filter(person => person.name.toLowerCase().includes(filterString.toLowerCase()))
+            .map(person => <p key={person.id}>{person.name} {person.number} <button value={JSON.stringify(person)} type="button" onClick={deleteMethod}>delete</button></p>)
       }
     </>
   )
@@ -47,7 +60,28 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
+  const successStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    fontStyle: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  const errorStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontStyle: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
 
   const addName = (event) => {
     event.preventDefault()
@@ -57,7 +91,12 @@ const App = () => {
     }
 
     persons.find(x => x.name === newName) === undefined ?
-      PhoneService.create(personObject).then((result) => setPersons(persons.concat(result))) :
+      PhoneService.create(personObject)
+        .then((result) => {
+          setPersons(persons.concat(result))
+          setSuccessMessage(`Added ${newName}`)
+          setTimeout(() => setSuccessMessage(null), 5000)
+        }) :
       window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`) ?
         PhoneService.update(persons.find(x => x.name === newName).id, { ...persons.find(x => x.name === newName), number: newNumber })
           .then(() => PhoneService.getAll().then(result => setPersons(result))) :
@@ -79,9 +118,14 @@ const App = () => {
   }
 
   const deleteNumber = (event) => {
+    const person = JSON.parse(event.target.value)
     PhoneService
-      .remove(event.target.value)
-      .then(() => PhoneService.getAll().then(result => setPersons(result)))
+      .remove(person.id)
+      .then(() =>
+        PhoneService.getAll().then(result => setPersons(result)))
+      .catch(error => {
+        setErrorMessage(`${person.name} has already been removed from the server.`)
+      })
   }
 
   useEffect(() => {
@@ -91,6 +135,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={successMessage} style={successStyle}></Notification>
+      <Notification message={errorMessage} style={errorStyle}></Notification>
       <Filter string={filter} method={handleFilter}></Filter>
       <h2>Add a new</h2>
       <PersonForm submitMethod={addName} nameInput={newName} nameOnChange={handleNewName} numberInput={newNumber} numberOnChange={handleNewNumber}></PersonForm>
