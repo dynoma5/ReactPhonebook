46134653ffa660b11983cd58ac0750f0e0bceea4
David Seo <manakete659@gmail.com>
2020-09-16 13:49:56 -0400

    Added connection to database

---
+++ src/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.hs80f.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+const phoneSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', phoneSchema)
+
+if (process.argv.length == 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    person.save().then(result => {
+        console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
